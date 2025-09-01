4e17cf733418e19125559082bb3fdf82e18d05b7
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-09-01 18:57:07 -0500

    js works, also I got rid of DIST

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,7 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+  const savedItems = localStorage.getItem("selectedItems")
+  let selectedItems = savedItems===null ? [] : JSON.parse (savedItems)
 
 const dataSource = new ProductData("tents");
 
@@ -10,6 +12,10 @@ function addProductToCart(product) {
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
+  alert('a item has been added')
+  selectedItems.push(product)
+  let stringarray = JSON.stringify(selectedItems)
+  localStorage.setItem('selectedItems', stringarray);
 }
 
 // add listener to Add to Cart button

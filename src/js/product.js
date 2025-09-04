6d3b95d56cd511957492c75385c8b9dae1a9395f
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-09-04 09:58:35 -0500

    product js made the function hold the add to cart things

--- src/js/product.js
+++ src/js/product.js
@@ -7,13 +7,12 @@ const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
+  selectedItems.push(product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
-  alert('a item has been added')
-  selectedItems.push(product)
   let stringarray = JSON.stringify(selectedItems)
   localStorage.setItem('selectedItems', stringarray);
 }

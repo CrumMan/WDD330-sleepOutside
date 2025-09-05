4847fcb6c7b14bf8d1381a8b88bc9b58d8573c8d
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-09-05 08:20:18 -0500

    dunamic product list added

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+
+    const response = await fetch('./json/tents.json')
+    const tents = await response.json();
+    const template = document.getElementById('product-card');
+    tents.forEach((tent) => {
+        const clone = template.content.cloneNode(true);
+
+        const brand = clone.querySelector('.card__brand');
+        const name = clone.querySelector('.card__name');
+        const photo = clone.querySelector('.productImg');const lp = clone.querySelector('.product-card__price');
+        const href = clone.querySelector('a#href');
+        href.href = tent.Href.replace('../','');
+        brand.textContent = tent.Brand.Name;
+        name.textContent = tent.Name;
+        photo.src = tent.Image.replace('../','./');
+        lp.textContent = tent.ListPrice;
+        const productContainer = document.querySelector('.product-list');
+        productContainer.appendChild(clone);
+    });
+
+// ../product_pages/marmot-ajax-3.html

7fd06358000baa04d7e73740f225e245314305eb
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-09-11 10:39:18 -0500

    adding dist

---
+++ dist/assets/cart-CUliQgSZ.js
@@ -0,0 +1,14 @@
+import"./style-HlQJNEhF.js";function r(a){return JSON.parse(localStorage.getItem(a))}function c(){const t=r("selectedItems").map(e=>n(e));document.querySelector(".product-list").innerHTML=t.join("")}function n(a){return`<li class='cart-card divider'>
+  <a href='#' class='cart-card__image'>
+    <img
+      src='${a.Image}'
+      alt='${a.Name}'
+    />
+  </a>
+  <a href='#'>
+    <h2 class='card__name'>${a.Name}</h2>
+  </a>
+  <p class="cart-card__color">${a.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">Quantity: ${a.quantity} </p>
+  <p class="cart-card__price">$${a.FinalPrice}</p>
+</li>`}c();

4c02e07be83ea69b7463530e42e25fe3549a3579
Andy-8012 <andrewvriley@gmail.com>
2025-09-19 09:14:50 -0600

    Testing changes to product page on live server. Commented out old code and added new code

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage } from "./utils.mjs";
+/*import { setLocalStorage } from "./utils.mjs";
 const savedItems = new setLocalStorage("selectedItems");
 
 let selectedItems = savedItems === null ? [] : JSON.parse(savedItems);
@@ -30,4 +30,42 @@ setTimeout(() => {
   document
     .getElementById("addToCart")
     .addEventListener("click", addToCartHandler);
+}, 100);*/
+
+
+
+
+
+
+
+
+
+//Test different code
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+
+let selectedItems = getLocalStorage("selectedItems") ?? [];
+
+const dataSource = new ProductData("tents");
+
+function addProductToCart(product) {
+  const existingItem = selectedItems.find((item) => item.Id === product.Id);
+  if (!existingItem) {
+    product.quantity = 1;
+    selectedItems.push(product);
+  } else {
+    existingItem.quantity += 1;
+  }
+}
+
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+  setLocalStorage("selectedItems", selectedItems);
+}
+
+setTimeout(() => {
+  document
+    .getElementById("addToCart")
+    .addEventListener("click", addToCartHandler);
 }, 100);
+

8e3a7a7e96ba08b0d44eb294f66e88319df05c83
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-09-26 12:39:01 -0500

    trying to get it to work, added a few methods but cant figure it out yet...

--- src/js/checkpage.js
+++ src/js/checkpage.js
@@ -1,37 +1,7 @@
 import { getLocalStorage, loadHeaderFooter,populate_totals} from "./utils.mjs";
 import { packageItems} from "./CheckoutProcess.MJS";
-import CheckoutProcess from "./CheckoutProcess.MJS";
 
-const order = new CheckoutProcess("cartItems","#cart-totals")
-
-checkout_form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    order.CheckoutProcess();
-  })
-
-function cartItemTemplate(item) {
-    const newItem = `<li class='cart-card divider'>
-  <a href='#' class='cart-card__image'>
-    <img
-      src='${item.Images.PrimaryMedium}'
-      alt='${item.Name}'
-    />
-  </a>
-  <a href='#'>
-    <h2 class='card__name'>${item.Name}</h2>
-  </a>
-  <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
-  <p class='cart-card__quantity'>qty:${item.quantity}</p>
-  <p class='cart-card__price'>$${item.FinalPrice}</p>
-
-</li>`;
-
-    return newItem;
-}
-
-populate_totals("selectedItems");
-renderCartContents();
-loadHeaderFooter();
+const checkout_form = document.getElementById("checkout-form")
 
 function renderCartContents() {
     const cartItems = getLocalStorage("selectedItems");
@@ -45,17 +15,12 @@ function renderCartContents() {
     else {
         const htmlItems = cartItems.map(cartItemTemplate);
         productList.innerHTML = htmlItems.join("");
-        // if there are items in the cart make the total visible and get the total cost of the items in the cart
-        //cart_total_container.style.visibility = "visible";
-        //let total = 0;
-        //for (let item of cartItems) {
-        //    total = total + (item.FinalPrice * item.quantity);
-        //}
-        //display total cost
-        //cart_total.textContent = "Total: $" + total;
+        
     }
 
+function PopulatePostForm() {
 
+}
 
 
 }
